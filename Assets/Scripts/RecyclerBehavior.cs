b123d34c378ca4c44203f5fa236b11ff489098d2
Kelsea Canaday <150960092+KelseaWasHere@users.noreply.github.com>
2024-04-09 12:48:48 -0400

    Tross / Death Animations Added
    Began working on tross movement and death animations for both enemies. Still having issues with positioning of both enemies

--- Assets/Scripts/RecyclerBehavior.cs
+++ Assets/Scripts/RecyclerBehavior.cs
@@ -1,66 +1,18 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class RecyclerEnemy : MonoBehaviour
 {
-    public float proximityRange = 5f;
-    public float attackRange = 2f;
-    public float movementSpeed = 2f;
-
-    private Animator animator;
-    private Transform player;
-    private bool isPlayerInRange = false;
-
+    public int HP = 1;
+    public Animator animator;
    
-    void Start()
+    
+    public void TakeDamage(int damageAmount)
     {
-        animator = GetComponent<Animator>();
-        player = FindObjectOfType<PlayerController>().transform;
-        animator.Play("Recycler.Idle.Gnawing");
-    }
-
-    void Update()
-    {
-        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
-
-        animator.SetBool("ProximityRangeReached", isPlayerInRange);
-
-        animator.SetBool("AttackRangeReached", distanceToPlayer < attackRange);
-
-
-        if (!isPlayerInRange)
+        HP -= damageAmount;
+        if (HP <= 0)
         {
-
-            animator.Play("Recycler.Idle.Gnawing");
-
-            if (distanceToPlayer < proximityRange)
-            {
-                isPlayerInRange = true;
-                animator.SetTrigger("PlayerInRange");
-            }
+            animator.SetTrigger("Die");
         }
-        else
-        {
-            animator.Play("Recycler.Walk.Forward");
-            transform.LookAt(player);
-
-            if (distanceToPlayer < attackRange)
-            {
-                animator.Play("Recycler.Attack.Standard");
-                player.GetComponent<PlayerController>().TakeDamage();
-            }
-            else
-            {
-                transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
-            }
-        }
-    }
-
-    public void TakeDamage()
-    {
-        //dies after one hit
-        isPlayerInRange = false;
-        animator.Play("Recycler.Damage.Die");
-
-        Destroy(gameObject, 2f); 
     }
 }
